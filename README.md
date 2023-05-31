66360128016c15ae67e2cfe2257c85fbea023e91
Yanik Kumar <believemaster@gmail.com>
2023-05-31 22:21:04 +0530

    Created admin middleware

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Simple Role Based Authentication For Laravel Projects
 > Created Laravel Project Configure DB.
 > Installed Laravel Breeze for Authentication Scaffolding.
 > Created DB Seeder for Admin
+> Creates Admin middleware
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Admin Dashboard') }}
+        </h2>
+    </x-slot>
+
+</x-admin-layout>

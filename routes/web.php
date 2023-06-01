4ca131ec58acb2223ab7c813df692144ff8059b4
Yanik Kumar <believemaster@gmail.com>
2023-06-01 21:21:04 +0530

    Added admin layout for dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,7 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['is_admin'])->group(function() {
+Route::middleware(['auth'])->group(function() {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
 });
 

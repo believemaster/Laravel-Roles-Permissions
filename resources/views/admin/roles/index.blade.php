fd10afc5596c158bab91cca37337140b4d3932b3
Yanik Kumar <believemaster@gmail.com>
2023-06-05 21:30:55 +0530

    Displaying Roles & Permissions in dashboard

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,3 +1,36 @@
 <x-admin-layout>
-    Roles Admin File
+
+    <div class="mt-12 max-w-6xl mx-auto">
+        <div class="relative overflow-x-auto shadow-md bg-gray-200 sm:rounded-lg">
+            <table class="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+                <thead class="text-xs bg-gray-50 uppercase dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">Id</th>
+                        <th scope="col" class="px-6 py-3">Name</th>
+
+                        <th scope="col" class="px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($roles as $role)
+                        <tr class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <th scope="row"
+                                class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                {{ $role->id }}</th>
+                            <th scope="row"
+                                class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                {{ $role->name }}</th>
+                            <td class="px-6 py-4 text-right">
+                                <a href=""
+                                    class="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
 </x-admin-layout>
